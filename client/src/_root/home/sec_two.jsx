935c78cf0d53618efd85906c30f914b5ce8f5dd5
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-07-06 14:31:08 +0100

    update PR

--- client/src/_root/home/sec_two.jsx
+++ client/src/_root/home/sec_two.jsx
@@ -4,22 +4,17 @@ function Section_Two() {
   return (
     <section className="flex w- py-[5em] md:py-[2em] mx-5 md:mx-24 md:flex-row flex-col gap-5 items-start justify-between">
       <div className="flex flex-row gap-3 p-5 bg-[#eeb88184] rounded-md items-start justify-center">
-        <div className="flex flex-col md:mt-0 mt-6 justify-center items-start">
-          <div className="font-bold">Ring in Yellow Gold with Diamonds</div>
-        </div>
+        <div className="font-bold">Ring in Yellow Gold with Diamonds</div>
         <img
           className="w-full h-[120px] relative"
           src="/assets/imgs/ring-1.png"
         />
       </div>
 
-      <div className="flex flex-row gap-3 p-5 bg-[#eeb88184] rounded-md items-center justify-center">
-        <div className="flex flex-col md:mt-0 mt-6 justify-center items-start">
-          <div className="font-bold">Flexible payment</div>
-          <div>Diffirent finance opitons will be available</div>
-        </div>
+      <div className="flex flex-row gap-3 p-5 bg-[#eeb88184] rounded-md items-start justify-center">
+        <div className="font-bold">Ring in Yellow Gold with Diamonds</div>
         <img
-          className="w-[60px] h-[60px] relative"
+          className="w-full h-[120px] relative"
           src="/assets/imgs/ring-1.png"
         />
       </div>
