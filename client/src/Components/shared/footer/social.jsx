426bd81a0316bff757645f86516ef54605ffdfd9
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-07-10 11:51:36 +0100

    update PR

--- client/src/Components/shared/footer/social.jsx
+++ client/src/Components/shared/footer/social.jsx
@@ -3,20 +3,30 @@ import { Link } from "react-router-dom";
 
 function Social() {
   return (
-    <div>
-      <img className="w-full h-[30rem] -z-20" src="/assets/imgs/newletters.png" />
-      <div className="flex absolute hidden mt-0 bottom-0 mx-5 md:mx-12 z-50 flex-col items-start justify-center">
-        <div className="text-primary text-[35px] font-bold font-['Cormorant']">
+    <div className="flex bg-[#303741] items-center justify-center">
+      <div className="flex flex-col gap-3 ml-12 py-4 items-start justify-center">
+        <div className="text-primary text-[15px] font-bold font-['Cormorant']">
           $20 discount for your first order
         </div>
-        <div className="text-white text-6xl font-bold font-['Cormorant']">
+        <div className="text-white text-3xl font-bold font-['Cormorant']">
           Join our newsletter and get...
         </div>
-        <div className="text-white text-3xl font-bold font-['Cormorant']">
+        <div className="text-white text-2xl font-['Cormorant']">
           We'll email you a voucher worth $10 off your first order over $50.
           <br />
         </div>
+
+        <div className="flex items-center rounded-md">
+          <input
+            className="text-black w-full p-3 border lg:w-[15em] outline-primary rounded-l-md"
+            placeholder="Email address"
+          />
+          <button className="font-bold bg-primary text-white hover:bg-white hover:text-primary py-3 text-center w-full lg:w-[10em] rounded-r-md">
+          SUBSCRIBE
+        </button>
+        </div>
       </div>
+      <img className="w-[80vw] h-[70vh]" src="/assets/imgs/newletters.png" />
     </div>
   );
 }
