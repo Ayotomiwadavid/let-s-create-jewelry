cc6e213b0ee0b5d3ff02f6dcab2e92e60276c6c6
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-07-13 02:32:35 +0100

    update PR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./Auths/signin";
 import SignUp from "./Auths/signup";
-import Home from "./_root/Home/home";
+import Home from "./_root/Home/index";
 import Cart from "./_root/cart/index";
 import CheckOut from "./_root/CheckOut/index";
 import Contact from "./_root/Contact/index";
