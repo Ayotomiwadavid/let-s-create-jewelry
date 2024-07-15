ae50d288166b950e70321cddfb5fb83f5d853c66
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-07-15 09:49:30 +0100

    update PR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./Components/Auths/signin";
 import SignUp from "./Components/Auths/signup";
-import Home from "./Components/root/home/index";
-import Cart from "./Components/root/cart/index";
-import CheckOut from "./Components/root/CheckOut/index";
-import Contact from "./Components/root/Contact/index";
-import Shop from "./Components/root/shop/index";
-import ProductDetail from "./Components/root/productDetail/index";
+import Home from "./Components/_root/home/index";
+import Cart from "./Components/_root/cart/index";
+import CheckOut from "./Components/_root/CheckOut/index";
+import Contact from "./Components/_root/Contact/index";
+import Shop from "./Components/_root/shop/index";
+import ProductDetail from "./Components/_root/productDetail/index";
 import ScrollToTop from "./BackToTop/ScrollToTop";
 
 function App() {
